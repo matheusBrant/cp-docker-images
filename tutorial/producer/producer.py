47a802bda506542ad019be78b85f894add9298a8
matheusBrant <matheusbrantgo@gmail.com>
2021-11-25 18:25:30 -0300

    criando consumer/producer

--- tutorial/producer/producer.py
+++ tutorial/producer/producer.py
@@ -5,10 +5,15 @@ from time import sleep
 from datetime import datetime
 
 # Create an instance of the Kafka producer
-producer = KafkaProducer(bootstrap_servers='localhost:29092',
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
                             value_serializer=lambda v: str(v).encode('utf-8'))
 
 # Call the producer.send method with a producer-record
 print("Ctrl+c to Stop")
 while True:
     producer.send('kafka-python-topic', random.randint(1,999))
+
+
+# create topic 
+# sudo docker-compose exec kafka \
+# kafka-topics --create --topic kafka-python-topic --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092
